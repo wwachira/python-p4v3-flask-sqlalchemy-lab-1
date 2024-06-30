cfa88a1b8f634fe56865650d393df416dbfee638
lizwachira <lizwanguwachira@gmail.com>
2024-06-30 05:27:39 +0300

    returns a JSON response: tests pass

--- server/app.py
+++ server/app.py
@@ -21,6 +21,21 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route('/earthquakes/<int:id>')
+def earthquake_by_id(id):
+    earthquake = Earthquake.query.get(id)
+    if earthquake:
+        body = {
+            'id': earthquake.id,
+            'location': earthquake.location,
+            'magnitude': earthquake.magnitude,
+            'year': earthquake.year
+        }
+        status = 200
+    else:
+        body = {'message': f'Earthquake {id} not found.'}
+        status = 404
+    return make_response(body, status)
 
 
 if __name__ == '__main__':
